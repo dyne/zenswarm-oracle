9a844f853ddd4f5d50a56e3df85b4bd49a6dcaf8
Alberto Lerda <albertolerda97@gmail.com>
2022-07-05 15:03:55 +0200

    fix: change permissions

--- init.mjs
+++ init.mjs
@@ -42,6 +42,11 @@ Object.assign(keyring, JSON.parse(keys.result))
 await fsp.writeFile(
     path.join(ZENCODE_DIR, "zenswarm-oracle-generate-all-public-keys.keys"),
     keys.result)
+// Delete keyring if it exists (writing a file that exist
+// doesn't change the permissions)
+try {
+    await fsp.unlink(path.join(ZENCODE_DIR, "keyring.json"))
+} catch(e) {}
 await fsp.writeFile(
     path.join(ZENCODE_DIR, "keyring.json"),
     JSON.stringify(keyring), {mode: 0o600})
